0a1ecd46ab71de6ca8abe8cdfb4f42a4c921a028
Sam Ardis <ardis.productions@gmail.com>
2020-04-24 11:36:37 -0400

    Added auth-router code and a few other files, working on testing and the remaining files

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,25 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secret');
+
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  const secret = secrets.jwtSecret;
+  
+  if(token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if(err) {
+        res.status(401).json({ message: 'error, you shall not pass!' })
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ message: 'provide credentials' })
+  };
 };
